1648a5151bf4f2d6d525b58c829f005ac5c65991
nmcain <nmcain@github.com>
2018-10-25 06:35:38 +0000

    upload

---
+++ renderer.js
@@ -0,0 +1,32 @@
+   function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m ;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    //document.getElementById("msg").innerHTML = "Enabling component jslinux-nocrostini_1.1"
+  await sleep(1000);
+  
+  
+    document.getElementById("loadscreen").style.display = "none"
+}
+demo();
